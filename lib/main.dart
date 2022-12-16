3bd6048815a82d5564c297475138affc7a8a4a0e
AkmalSab <akmalsabri@outlook.com>
2022-12-16 22:52:19 +0800

    #8 - Images & Assets

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,9 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.amber[400],
       ),
       body: Center(
-        child: Text(
-          'Hello Ninjas !',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        child: Image.asset('assets/1.jpg'),
+          // image: NetworkImage('https://images.unsplash.com/photo-1664574654700-75f1c1fad74e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+          // image: AssetImage('assets/3.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
